8accaebcd8e944d66498b4789aee520072e39dc1
Igor Unanua <igor.unanua@datadoghq.com>
2025-03-17 16:06:43 +0100

    Include trace propagation code from datadog-lambda-extension

---
+++ dd-trace-propagation/src/config.rs
@@ -0,0 +1,34 @@
+// Copyright 2025-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
+use serde::Deserialize;
+
+use crate::trace_propagation_style::{deserialize_trace_propagation_style, TracePropagationStyle};
+
+#[derive(Debug, PartialEq, Deserialize, Clone)]
+#[serde(default)]
+#[allow(clippy::struct_excessive_bools)]
+pub struct Config {
+    // Trace Propagation
+    #[serde(deserialize_with = "deserialize_trace_propagation_style")]
+    pub trace_propagation_style: Vec<TracePropagationStyle>,
+    #[serde(deserialize_with = "deserialize_trace_propagation_style")]
+    pub trace_propagation_style_extract: Vec<TracePropagationStyle>,
+    pub trace_propagation_extract_first: bool,
+    pub trace_propagation_http_baggage_enabled: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            // Trace Propagation
+            trace_propagation_style: vec![
+                TracePropagationStyle::Datadog,
+                TracePropagationStyle::TraceContext,
+            ],
+            trace_propagation_style_extract: vec![],
+            trace_propagation_extract_first: false,
+            trace_propagation_http_baggage_enabled: false,
+        }
+    }
+}
