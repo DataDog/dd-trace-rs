81e73605170833eb3ca29f4bcd826a11a89275eb
paullegranddc <82819397+paullegranddc@users.noreply.github.com>
2025-04-11 13:11:23 +0200

    feat(datadog-opentelemetry): add a span exporter that uses the libdatadog trace exporter (#8)
    # What does this PR do?
    
    * Add a `datadog-opentelemetry` crate in the workspace
    * This crate contains a `DatadogSpanExporter` which sends otel spans to
    the trace agent using the TraceExporter
    * This crate also contains code doing (an incomplete see TODOs)
    conversion from otel span data to datadog spans
      * An example of setting up the "datadog distribution" of the otel sdk
    
    The integration of the TraceExporter in the SpanExporter is a bit
    complex, and can probably be optimized.
    We spawn a new thread, and send span data to be sent or shutdown
    requests through a channel. The worker then sends back send results back
    to the span exporter though another channel.
    This is done because the current APIs exposed for
    SpanProcessor/SpanExporter in otel sdks are pretty bad and lead to
    crashes/deadlocks if used improperly within an async context.
    (See comment on `DatadogExporter`)
    
    # Motivation
    
    What inspired you to submit this pull request?
    
    # Additional Notes
    
    Anything else we should know when reviewing?
    
    [APMSP-1932]
    
    [APMSP-1932]:
    https://datadoghq.atlassian.net/browse/APMSP-1932?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ datadog-opentelemetry/examples/simple_tracing.rs
@@ -0,0 +1,27 @@
+// Copyright 2025-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
+use opentelemetry::trace::Tracer;
+use opentelemetry_sdk::trace::SdkTracerProvider;
+
+fn foo() {
+    opentelemetry::global::tracer("foo").in_span("foo", |_cx| {
+        println!("foo");
+        bar()
+    })
+}
+
+fn bar() {
+    opentelemetry::global::tracer("bar").in_span("bar", |_cx| println!("bar"))
+}
+
+fn main() {
+    let tracer_provider = datadog_opentelemetry::init_datadog(
+        dd_trace::Config::default(),
+        SdkTracerProvider::builder(),
+    );
+
+    foo();
+
+    tracer_provider.shutdown().expect("tracer shutdown failed");
+}
---
+++ dd-trace/src/constants.rs
@@ -0,0 +1,6 @@
+// Copyright 2025-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
+pub const HIGHER_ORDER_TRACE_ID_BITS_TAG: &str = "_dd.p.tid";
+pub const SPAN_KIND_TAG: &str = "span.kind";
+pub const SAMPLING_PRIORITY_TAG: &str = "_sampling_priority_v1";
