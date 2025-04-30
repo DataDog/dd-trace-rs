97208ed29db135dd32014f8de8f4d7f63ff06bd5
Igor Unanua <igor.unanua@datadoghq.com>
2025-04-30 16:24:59 +0200

    get tracestate value from otel extractor instead of from DD SpanContext

--- datadog-opentelemetry/src/text_map_propagator.rs
+++ datadog-opentelemetry/src/text_map_propagator.rs
@@ -114,7 +114,7 @@ impl TextMapPropagator for DatadogPropagator {
             .extract(&ExtractorWrapper { extractor })
             .map(|dd_span_context| {
                 let trace_flags = extract_trace_flags(&dd_span_context);
-                let trace_state = extract_trace_state(&dd_span_context);
+                let trace_state = extract_trace_state(extractor);
 
                 let otel_span_context = opentelemetry::trace::SpanContext::new(
                     opentelemetry::TraceId::from(dd_span_context.trace_id),
@@ -151,12 +151,13 @@ fn extract_trace_flags(sc: &SpanContext) -> opentelemetry::TraceFlags {
     }
 }
 
-fn extract_trace_state(sc: &SpanContext) -> opentelemetry::trace::TraceState {
+fn extract_trace_state(
+    extractor: &dyn opentelemetry::propagation::Extractor,
+) -> opentelemetry::trace::TraceState {
     // TODO: we are parsing twice tracestate
-    match sc.tags.get(TRACESTATE_KEY) {
-        Some(trace_state) => {
-            opentelemetry::trace::TraceState::from_str(trace_state).unwrap_or_default()
-        }
+    match extractor.get(TRACESTATE_KEY) {
+        Some(trace_state) => opentelemetry::trace::TraceState::from_str(trace_state)
+            .unwrap_or_else(|_| opentelemetry::trace::TraceState::default()),
         None => opentelemetry::trace::TraceState::default(),
     }
 }
