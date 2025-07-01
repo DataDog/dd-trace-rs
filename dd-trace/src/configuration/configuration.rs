2437c74b6bebfb55e752eee96b175d8457af885a
Zachary Groves <32471391+ZStriker19@users.noreply.github.com>
2025-07-01 10:37:28 -0400

    feat(sampler): add stats computation envar and config, default to true (#38)
    # What does this PR do?
    adds envar DD_TRACE_STATS_COMPUTATION_ENABLED so that our stats_enabled
    can be toggled, which also toggles whether or not we send dropped spans
    
    
    # Motivation
    We need to have the ability to toggle this configuration, since unlike
    other tracers, dd-trace-rs defaults to True, therefore for system-tests,
    we can't actually check the span values for dropped spans. By being able
    to set DD_TRACE_STATS_COMPUTATION_ENABLED to false in the system-tests
    we can then test against spans that would have otherwise not been sent.
    
    # Additional Notes
    
    Anything else we should know when reviewing?
    
    ---------
    
    Signed-off-by: ZStriker19 <zach.groves@datadoghq.com>

--- dd-trace/src/configuration/configuration.rs
+++ dd-trace/src/configuration/configuration.rs
@@ -168,6 +168,10 @@ pub struct Config {
     /// The log level filter for the tracer
     log_level_filter: LevelFilter,
 
+    /// Whether to enable stats computation for the tracer
+    /// Results in dropped spans not being sent to the agent
+    trace_stats_computation_enabled: bool,
+
     /// Configurations for testing. Not exposed to customer
     #[cfg(feature = "test-utils")]
     wait_agent_info_ready: bool,
@@ -235,6 +239,10 @@ impl Config {
             enabled: to_val(sources.get_parse("DD_TRACE_ENABLED")).unwrap_or(default.enabled),
             log_level_filter: to_val(sources.get_parse("DD_LOG_LEVEL"))
                 .unwrap_or(default.log_level_filter),
+            trace_stats_computation_enabled: to_val(
+                sources.get_parse("DD_TRACE_STATS_COMPUTATION_ENABLED"),
+            )
+            .unwrap_or(default.trace_stats_computation_enabled),
             trace_propagation_style: TracePropagationStyle::from_tags(
                 to_val(sources.get_parse::<DdTags>("DD_TRACE_PROPAGATION_STYLE"))
                     .map(|DdTags(tags)| Some(tags))
@@ -322,6 +330,10 @@ impl Config {
         &self.log_level_filter
     }
 
+    pub fn trace_stats_computation_enabled(&self) -> bool {
+        self.trace_stats_computation_enabled
+    }
+
     #[cfg(feature = "test-utils")]
     pub fn __internal_wait_agent_info_ready(&self) -> bool {
         self.wait_agent_info_ready
@@ -369,6 +381,7 @@ impl Default for Config {
             log_level_filter: LevelFilter::default(),
             tracer_version: TRACER_VERSION,
             language_version: "TODO: Get from env",
+            trace_stats_computation_enabled: true,
             #[cfg(feature = "test-utils")]
             wait_agent_info_ready: false,
 
@@ -464,6 +477,14 @@ impl ConfigBuilder {
 
     pub fn set_log_level_filter(&mut self, filter: LevelFilter) -> &mut Self {
         self.config.log_level_filter = filter;
+        self
+    }
+
+    pub fn set_trace_stats_computation_enabled(
+        &mut self,
+        trace_stats_computation_enabled: bool,
+    ) -> &mut Self {
+        self.config.trace_stats_computation_enabled = trace_stats_computation_enabled;
         self
     }
 
@@ -741,5 +762,37 @@ mod tests {
             Some(vec![TracePropagationStyle::TraceContext]).as_deref()
         );
         assert!(config.trace_propagation_extract_first());
+    }
+
+    #[test]
+    fn test_stats_computation_enabled_config() {
+        let mut sources = CompositeSource::new();
+        sources.add_source(HashMapSource::from_iter(
+            [("DD_TRACE_STATS_COMPUTATION_ENABLED", "false")],
+            ConfigSourceOrigin::EnvVar,
+        ));
+        let config = Config::builder_with_sources(&sources).build();
+        assert!(!config.trace_stats_computation_enabled());
+
+        let mut sources = CompositeSource::new();
+        sources.add_source(HashMapSource::from_iter(
+            [("DD_TRACE_STATS_COMPUTATION_ENABLED", "true")],
+            ConfigSourceOrigin::EnvVar,
+        ));
+        let config = Config::builder_with_sources(&sources).build();
+        assert!(config.trace_stats_computation_enabled());
+
+        let mut sources = CompositeSource::new();
+        sources.add_source(HashMapSource::from_iter(
+            [("DD_TRACE_STATS_COMPUTATION_ENABLED", "a")],
+            ConfigSourceOrigin::EnvVar,
+        ));
+        let config = Config::builder_with_sources(&sources).build();
+        assert!(config.trace_stats_computation_enabled());
+
+        let mut builder = Config::builder();
+        builder.set_trace_stats_computation_enabled(false);
+        let config = builder.build();
+        assert!(!config.trace_stats_computation_enabled());
     }
 }
