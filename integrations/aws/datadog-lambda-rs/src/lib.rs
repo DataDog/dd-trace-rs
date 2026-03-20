d34666c5f6ea5d8d542c6d681e695e4034b0b835
David Ogbureke <david.ogbureke@datadoghq.com>
2026-03-20 17:01:05 -0400

    refactor(datadog-lambda-rs): remove tower layer to focus on core api

--- integrations/aws/datadog-lambda-rs/src/lib.rs
+++ integrations/aws/datadog-lambda-rs/src/lib.rs
@@ -6,16 +6,11 @@ mod invocation;
 mod span_inferrer;
 
 use invocation::{run_in_invocation_scope, start_invocation};
-use lambda_runtime::{
-    tower::{Layer, Service},
-    LambdaEvent,
-};
+use lambda_runtime::LambdaEvent;
 use opentelemetry_sdk::trace::SdkTracerProvider;
 use serde::de::DeserializeOwned;
 use serde_json::Value;
-use std::marker::PhantomData;
 use std::sync::Arc;
-use std::task::{Context as TaskContext, Poll};
 
 type BoxFuture<T> = std::pin::Pin<Box<dyn std::future::Future<Output = T> + Send>>;
 type HandlerError = lambda_runtime::Error;
@@ -58,92 +53,15 @@ where
         let config = Arc::clone(&config);
         let scope = start_invocation(&event, &provider, &config);
         let typed_payload = match serde_json::from_value::<E>(event.payload) {
-            Ok(p) => p,
-            Err(e) => {
+            Ok(payload) => payload,
+            Err(err) => {
                 return Box::pin(async move {
-                    run_in_invocation_scope(scope, provider, async move { Err(e.into()) }).await
-                })
+                    run_in_invocation_scope(scope, provider, async move { Err(err.into()) }).await
+                });
             }
         };
         let typed_event = LambdaEvent::new(typed_payload, event.context);
         let fut = handler(typed_event);
-        Box::pin(async move { run_in_invocation_scope(scope, provider, fut).await })
-    }
-}
-
-/// Tower `Layer` that applies Datadog Lambda tracing to a `tower::Service`.
-pub struct DatadogLambdaLayer {
-    provider: SdkTracerProvider,
-    config: Arc<Config>,
-}
-
-impl DatadogLambdaLayer {
-    pub fn new(provider: SdkTracerProvider) -> Self {
-        Self {
-            provider,
-            config: Arc::new(Config::default()),
-        }
-    }
-
-    pub fn with_config(provider: SdkTracerProvider, config: Config) -> Self {
-        Self {
-            provider,
-            config: Arc::new(config),
-        }
-    }
-}
-
-impl<S> Layer<S> for DatadogLambdaLayer {
-    type Service = DatadogLambdaService<S>;
-
-    fn layer(&self, inner: S) -> Self::Service {
-        DatadogLambdaService {
-            inner,
-            provider: self.provider.clone(),
-            config: Arc::clone(&self.config),
-            _phantom: PhantomData,
-        }
-    }
-}
-
-#[doc(hidden)]
-pub struct DatadogLambdaService<S, E = Value> {
-    inner: S,
-    provider: SdkTracerProvider,
-    config: Arc<Config>,
-    _phantom: PhantomData<fn(LambdaEvent<E>)>,
-}
-
-impl<S, E> Service<LambdaEvent<Value>> for DatadogLambdaService<S, E>
-where
-    S: Service<LambdaEvent<E>> + Send + 'static,
-    S::Future: Send + 'static,
-    S::Response: Send + 'static,
-    S::Error: From<serde_json::Error> + std::fmt::Display + Send + 'static,
-    E: DeserializeOwned + Send + Sync + 'static,
-{
-    type Response = S::Response;
-    type Error = S::Error;
-    type Future = BoxFuture<Result<S::Response, S::Error>>;
-
-    fn poll_ready(&mut self, cx: &mut TaskContext<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
-    }
-
-    fn call(&mut self, event: LambdaEvent<Value>) -> Self::Future {
-        let provider = self.provider.clone();
-        let config = Arc::clone(&self.config);
-        let scope = start_invocation(&event, &provider, &config);
-        let typed_payload = match serde_json::from_value::<E>(event.payload) {
-            Ok(p) => p,
-            Err(e) => {
-                return Box::pin(async move {
-                    run_in_invocation_scope(scope, provider, async move { Err(e.into()) }).await
-                })
-            }
-        };
-        let typed_event = LambdaEvent::new(typed_payload, event.context);
-        let fut = self.inner.call(typed_event);
         Box::pin(async move { run_in_invocation_scope(scope, provider, fut).await })
     }
 }
