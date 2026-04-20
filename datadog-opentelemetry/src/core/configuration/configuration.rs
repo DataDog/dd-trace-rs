ae2e099a9f9a111fc49d92c5436dccaf5990386e
Yann Hamdaoui <yann.hamdaoui@datadoghq.com>
2026-04-20 15:16:46 +0200

    feat: add missing container_id and hostname to process context

--- datadog-opentelemetry/src/core/configuration/configuration.rs
+++ datadog-opentelemetry/src/core/configuration/configuration.rs
@@ -1614,14 +1614,37 @@ impl Config {
     /// Generate tracer metadata from this config.
     #[cfg(target_os = "linux")]
     pub(crate) fn to_tracer_metadata(&self) -> TracerMetadata {
+        fn hostname() -> String {
+            let mut buf = Vec::with_capacity(256);
+
+            unsafe {
+                // Safety: buf is valid for writes for at most buf.len().
+                if libc::gethostname(buf.as_mut_ptr() as *mut libc::c_char, buf.len()) == 0 {
+                    // Amusingly (so to speak), if the host name doesn't fit in `buf.len()`,
+                    // gethostname will put a truncated version in the buffer, which isn't
+                    // null-terminated. So the resulting buffer might or might not be a valid C
+                    // string...
+                    let len = buf.iter().position(|&c| c == 0).unwrap_or(buf.len());
+                    buf.truncate(len);
+                    // Note: use from_utf8_lossy_owned once it's stabilized
+                    String::from_utf8(buf)
+                        .unwrap_or_else(|err| String::from_utf8_lossy(err.as_bytes()).into_owned())
+                } else {
+                    String::new()
+                }
+            }
+        }
+
         TracerMetadata {
             runtime_id: Some(self.runtime_id.to_owned()),
             tracer_language: "rust".to_owned(),
             tracer_version: self.tracer_version.to_owned(),
+            hostname: hostname(),
             service_name: Some(self.service().to_owned()),
             service_env: self.env().map(str::to_owned),
             service_version: self.version().map(str::to_owned),
-            // What about hostname, process_tags and container_id ?
+            container_id: libdd_common::entity_id::get_container_id().map(str::to_owned),
+            // TODO: add the process tags. For now, we can't easily get them.
             ..Default::default()
         }
     }
